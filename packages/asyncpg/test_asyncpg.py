73ba917fe4b534e8035dbe72ad0352b67c19acfd
Gyeongjae Choi <gyeongjae@cloudflare.com>
2026-09-11 13:52:03 +0900

    tidy up asyncpg test

--- packages/asyncpg/test_asyncpg.py
+++ packages/asyncpg/test_asyncpg.py
@@ -3,11 +3,11 @@ from pytest_pyodide import run_in_pyodide
 
 @run_in_pyodide(packages=["asyncpg"])
 def test_asyncpg_native_modules(selenium):
-    import asyncpg
+    # Just a smoke test.
+    # see e2e tests for a more comprehensive test
     from asyncpg.pgproto import pgproto
     from asyncpg.protocol import protocol, record
 
-    assert asyncpg.__version__ == "0.31.0"
     assert pgproto.__file__.endswith(".so")
     assert protocol.__file__.endswith(".so")
     assert record.__file__.endswith(".so")
